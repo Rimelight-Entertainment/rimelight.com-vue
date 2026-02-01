12fc6908143c638b93f3f904e6a8fecaf76ce52b
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-02-01 16:49:02 -0300

    adds new linting rule for formatting iconify icons

--- .oxlint-plugins/rimelight.js
+++ .oxlint-plugins/rimelight.js
@@ -1,13 +1,15 @@
 import {definePlugin} from "oxlint"
 import {componentPropsStandard} from "./rules/componentPropsStandard.js"
 import {preferValidatedGetters} from "./rules/preferValidatedGetters.js"
+import {iconifyStandardFormat} from "./rules/iconifyStandardFormat.js"
 
 export default definePlugin({
-    meta: {
-        name: "rimelight"
-    },
-    rules: {
-        "component-props-standard": componentPropsStandard,
-        "prefer-validated-getters": preferValidatedGetters
-    }
+  meta: {
+    name: "rimelight"
+  },
+  rules: {
+    "component-props-standard": componentPropsStandard,
+    "prefer-validated-getters": preferValidatedGetters,
+    "iconify-standard-format": iconifyStandardFormat
+  }
 })
