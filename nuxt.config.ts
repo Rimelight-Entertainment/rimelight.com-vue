2dfe016e91742f985ce3f0d3de3e4e648a61c58c
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-03-06 16:33:33 -0300

    feat: adds nuxt security module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,7 @@ export default defineNuxtConfig({
     "@pinia/colada-nuxt",
     "@nuxt/a11y",
     "@nuxt/scripts",
-    ...(!isTauri ? ["@nuxtjs/sitemap", "@nuxtjs/robots", "nuxt-og-image"] : []),
+    ...(!isTauri ? ["@nuxtjs/sitemap", "@nuxtjs/robots", "nuxt-og-image", "nuxt-security"] : []),
   ],
 
   ignore: ["**/src-tauri/**"],
@@ -182,6 +182,47 @@ export default defineNuxtConfig({
   },
   ...(!isTauri
     ? {
+        security: {
+          ssg: {
+            meta: false,
+            exportToPresets: false,
+          },
+          headers: {
+            contentSecurityPolicy: {
+              "img-src": [
+                "'self'",
+                "data:",
+                "blob:",
+                "https://cdn.rimelight.com",
+                "https://via.placeholder.com",
+                "https://ui.nuxt.com",
+              ],
+              "script-src": ["'self'", "'unsafe-inline'", "'wasm-unsafe-eval'"],
+              "script-src-attr": ["'none'"],
+              "connect-src": [
+                "'self'",
+                "https://cdn.rimelight.com",
+                "https://api.iconify.design",
+                "https://api.unisvg.com",
+                "https://api.simplesvg.com",
+              ],
+              "font-src": ["'self'", "https://fonts.gstatic.com"],
+              "style-src": ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
+              "frame-ancestors": ["'self'"],
+              "form-action": ["'self'"],
+              "require-trusted-types-for": "'script'",
+            },
+            strictTransportSecurity: {
+              maxAge: 31536000,
+              includeSubdomains: true,
+              preload: true,
+            },
+            crossOriginOpenerPolicy: "same-origin",
+            referrerPolicy: "strict-origin-when-cross-origin",
+            xFrameOptions: "SAMEORIGIN",
+            xContentTypeOptions: "nosniff",
+          },
+        },
         site: {
           url: "https://rimelight.com",
           name: "Rimelight Entertainment",
